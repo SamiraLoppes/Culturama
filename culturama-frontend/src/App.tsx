458869a4a4676ca40de3e133b3bf47f86908758e
Markimg22 <marquim.net.221200@hotmail.com>
2025-04-29 19:32:29 -0400

    config tela Login

--- culturama-frontend/src/App.tsx
+++ culturama-frontend/src/App.tsx
@@ -18,14 +18,13 @@ import { Login } from './pages/Login';
 
 import '@ionic/react/css/core.css';
 import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
+import '@ionic/react/css/structure.css'
 import '@ionic/react/css/typography.css';
-
 
 setupIonicReact();
 
 export const App: React.FC = () => {
-    const [currentPage, setCurrentPage] = useState<string>("home");
+    const [currentPage, setCurrentPage] = useState<string>("login");
 
     return (
         <IonApp className='container'>
@@ -34,8 +33,9 @@ export const App: React.FC = () => {
                     <IonRouterOutlet>
                         <Route exact path="/" component={Home} />
                         <Route exact path="/favorites" component={Favorites} />
+                        <Route exact path="/login" component={Login} />
                     </IonRouterOutlet>
-                    <IonTabBar slot="bottom" color = "dark" className="rounded-tabbar">
+                    <IonTabBar slot="bottom" color="dark" className="rounded-tabbar">
                         <IonTabButton onClick={() => setCurrentPage('home')} selected={currentPage === 'home'} tab="home" href="/">
                             <IonIcon aria-hidden="true" icon={home} />
                         </IonTabButton>
@@ -43,7 +43,7 @@ export const App: React.FC = () => {
                             <IonIcon aria-hidden="true" icon={star} />
                         </IonTabButton>
                         <IonTabButton onClick={() => setCurrentPage('login')} selected={currentPage === 'login'} tab="login" href="/login">
-                         <IonIcon aria-hidden="true" icon={logIn} />
+                            <IonIcon aria-hidden="true" icon={logIn} />
                         </IonTabButton>
                     </IonTabBar>
                 </IonTabs>
--- culturama-frontend/src/pages/Login/index.tsx
+++ culturama-frontend/src/pages/Login/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import { Container } from '../../components/Container';
+
 export const Login: React.FC = () => {
-  return <h1>Olá</h1>;
+    return (
+        <Container>
+            <h1>Olá</h1>
+        </Container>
+    );
 };
